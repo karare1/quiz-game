14b1e861c48f9b0bb433e5e20f8b24c2762564dd
karare1 <defi20@yahoo.co.uk>
2022-12-28 19:38:39 +0000

    Move javascript code from html into a separate js file

---
+++ assets/js/topscore.js
@@ -0,0 +1,12 @@
+
+        const scoreTable = document.getElementById('score-table');
+        const topScore = JSON.parse(localStorage.getItem('topScore')) || [];
+    
+      //taking an array from topScore and converting into a new array of html li elements
+      scoreTable.innerHTML = topScore
+      .map(function (result) {
+          return `<li class='table-input'> ${result.userName}:\u00A0\u00A0\u00A0\u00A0 ${result.result}</li>`;
+      })
+      .join("");
+        
+    
