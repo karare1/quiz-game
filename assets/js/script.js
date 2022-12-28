e90ba6eba04527a3ff5fa5abe3b4ba4c1e44b666
karare1 <defi20@yahoo.co.uk>
2022-12-28 19:49:22 +0000

    Tidy up js structure for the home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,15 @@ window.onload;
 // clicking on About button will display pop-up window
 btnAbout.onclick = function() {
  popUp.style.display = "block";
-}
+};
 // clicking on x will close the pop-up window
 closeBox.onclick = function() {
   popUp.style.display = "none";
-}
+};
 
 // clicking anywhere outside of the pop-up window will close it
 window.onclick = function(event) {
   if (event.target == popUp) {
     popUp.style.display = "none";
-  }
-}
+  };
+};
